7d567bdc5dae440d9b8f34657de9bd2d697cafc0
Frank A. Antwi <fantwi@ucc.edu.gh>
2026-02-25 10:32:21 +0000

    Fixed the calendar view

--- resources/views/filament/admin/pages/booking-calendar.blade.php
+++ resources/views/filament/admin/pages/booking-calendar.blade.php
@@ -1,25 +1,21 @@
-<!-- <x-filament-panels::page>
-    {{-- Page content --}}
-</x-filament-panels::page> -->
-
 <x-filament-panels::page>
-    <div id="calendar"></div>
+    <div>
+        <div id="calendar"></div>
+    </div>
 
-    @push('scripts')
-        <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js"></script>
 
-        <script>
-            document.addEventListener('DOMContentLoaded', function () {
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
 
-                var calendarEl = document.getElementById('calendar');
+            var calendarEl = document.getElementById('calendar');
 
-                var calendar = new FullCalendar.Calendar(calendarEl, {
-                    initialView: 'dayGridMonth',
-                    events: '/admin/calendar-events',
-                });
-
-                calendar.render();
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth',
+                events: '/admin/calendar-events',
             });
-        </script>
-    @endpush
+
+            calendar.render();
+        });
+    </script>
 </x-filament-panels::page>
